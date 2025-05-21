6ec802cb1cc47ac56dc45624a428019dec205646
theusnevess <matheusneves1204@gmail.com>
2025-05-21 15:19:32 -0300

    Project created

---
+++ listaJogos/listaJogos/src/main/java/com/matheus/listaJogos/ListaJogosApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.listaJogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaJogosApplication.class, args);
+	}
+
+}
---
+++ listaJogos/listaJogos/src/test/java/com/matheus/listaJogos/ListaJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.listaJogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
